ce4a5eaef3e5ee0ba59d07e792157eee785389e5
super_wile_coyote@yahoo.ca <super_wile_coyote@yahoo.ca>
2022-09-08 02:45:36 -0400

    modified:   finder-app/manual-linux.sh

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,6 +35,20 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    # Note: Steps taken from lecture "Week 2: Building the Linux Kernel" 
+    echo "1) Clean the build"
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE mrproper
+    echo "2) Configure a virtualized Arm board"
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+    echo "3) Build a kernel image for booting with QEMU."
+    # Note: Needed to install "flex", "bison" and "libssl-dev" using apt to complete this phase
+    make -j4 ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE all
+    echo "4) Build kernel modules"
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE modules
+    echo "5) Build the devicetree binaries"
+    make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE dtbs
+    # TODO: End
+
 fi
 
 echo "Adding the Image in outdir"
@@ -48,6 +62,15 @@ then
 fi
 
 # TODO: Create necessary base directories
+# Note: Steps taken from lecture "Week 2: Linux Root Filesystem (6:25) and MELP (p.196) "
+echo "6) Create necessary base directories"
+mkdir rootfs
+cd "${OUTDIR}/rootfs"
+mkdir bin dev etc lib proc sys sbin tmp usr var home
+mkdir usr/bin usr/sbin
+mkdir usr/lib
+mkdir -p var/log
+# TODO: End
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -55,26 +78,69 @@ then
 git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+
+    # TODO: Configure busybox
+    # Note: Steps taken from lecture "Week 2: Linux Root Filesystem (9:35) and MELP (p.206)"
+    echo "7) Configure BusyBox"
+    make distclean
+    make defconfig
+    # TODO: End
+
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+# Note: Steps taken from lecture "Week 2: Linux Root Filesystem (10:45) and MELP (p.206)"
+echo "8) Make and install BusyBox"
+make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE CONFIG_PREFIX=${OUTDIR}/rootfs install
+cd "${OUTDIR}/rootfs"
+# TODO: End
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+echo "9) Add library dependencies"
+SYSROOT=`${CROSS_COMPILE}gcc -print-sysroot`
+sudo cp -a "${SYSROOT}"/lib/* lib/
+sudo cp -a "${SYSROOT}"/lib64 .
+# TODO: End
 
 # TODO: Make device nodes
+# Note: Steps taken from lecture "Week 2: Linux Root Filesystem (11:40)"
+# There are two devices you need to boot a device initially: 1) Null device 2) Console device
+echo "10) Make device nodes"
+sudo mknod -m 666 dev/null c 1 3 # Null device
+sudo mknod -m 666 dev/null c 5 1 # Console device.
+# TODO: End
 
 # TODO: Clean and build the writer utility
+echo "11) Clean and build the writer utility"
+cd $FINDER_APP_DIR
+make clean
+make CROSS_COMPILE=$CROSS_COMPILE
+# TODO: End
+
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+echo "12) Copy the finder scripts to the rootfs /home"
+cd $FINDER_APP_DIR
+cp -a writer autorun-qemu.sh finder-test.sh finder.sh "${OUTDIR}/rootfs/home"
+mkdir "${OUTDIR}/rootfs/home/conf"
+cp -a conf/username.txt "${OUTDIR}/rootfs/home/conf"
+# TODO: End
 
 # TODO: Chown the root directory
+echo "13) Chown the root directory"
+sudo chown -R root:root *
+# TODO: End
 
 # TODO: Create initramfs.cpio.gz
+echo "14) Create initramfs.cpio.gz file"
+cd "$OUTDIR/rootfs"
+find * . | cpio -o --format=newc > ../initramfs.cpio
+gzip -c ../initramfs.cpio > ../initramfs.cpio.gz
+# TODO: End
