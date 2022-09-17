2d04737fa39c21e47605d615726358d1d5b69a1c
super_wile_coyote@yahoo.ca <super_wile_coyote@yahoo.ca>
2022-09-17 03:07:52 -0400

    new file:   server/aesdsocket-start-stop.sh

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+
+# MELP p457
+case "$1" in
+    start)
+        echo "Starting simpelserver"
+        start-stop-daemon -S -n aesdsocket -a /usr/bin/simpleserver
+        ;;
+    stop)
+        echo "Stopping simpleserver"
+        start-stop-daemon -K -n aesdsocket
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+    exit 1
+esac
+
+exit 0
